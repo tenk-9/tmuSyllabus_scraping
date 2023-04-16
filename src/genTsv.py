847e992aeac1033543d3176d8bb610be86b2a14b
takuo yamaguchi <takuo@raspberrypi>
2023-04-17 01:12:33 +0900

    modified genTsv.py

--- src/genTsv.py
+++ src/genTsv.py
@@ -1,7 +1,7 @@
 import mysql.connector as mydb
 
 BASEINFO_TABLENAME="syllabus_base_info"
-
+LAST_UPDATED="20230416"
 connection = mydb.connect(host="localhost",
                           user="root",
                           password="PASSWORD",
@@ -11,7 +11,7 @@ connection = mydb.connect(host="localhost",
 cur = connection.cursor()
 cur.execute("SELECT * FROM {};".format(BASEINFO_TABLENAME))
 result = cur.fetchall()
-with open("../out.tsv", 'w', encoding="utf-8") as f:
+with open("out_{}.tsv".format(LAST_UPDATED), 'w', encoding="utf-8") as f:
     f.write("開講年度\t開講学期\t曜日\t時限\t教員\t科目名\t授業番号\t単位数\tシラバスURL\t科目区分\t学部コード\n")
     for col in result:
         for i in range(len(list(col))):
